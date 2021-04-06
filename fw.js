787c42edecdc34a379d55681e1f751839298faf3
Jeznon <82025124+grapecode@users.noreply.github.com>
2021-04-06 04:02:27 -0500

    Update Version2
    Version 2 File Watcher

--- fw.js
+++ fw.js
@@ -6,9 +6,9 @@ var dateFormat = require('dateformat');
 const fs = require('fs');
 fs.watch('./watch', function (event, filename) {
   if (event == 'change') {
-    var day = dateFormat(new Date(), 'dd-mm-yyyy_h-MM');
-    var fn = path.parse(filename);
-    const processedFile = './watch/' + fn.name + '_' + day + fn.ext;
+    var dateStr = dateFormat(new Date(), 'dd-mm-yyyy_h-MM');
+    var fileNameObject = path.parse(filename);
+    const processedFile = './watch/' + fileNameObject.name + '_' + dateStr + fileNameObject.ext;
     try {
       fs.renameSync('./watch/' + filename, processedFile);
     } catch (e) {}
